1fe9c5a1cad00de49bbc12c2d95377c7dd6d82b2
AdrianaPikaljov <pikaljovadri@gmail.com>
2025-02-04 15:31:06 +0200

    Update protseduur.sql

--- protseduur.sql
+++ protseduur.sql
@@ -59,5 +59,130 @@ end;
 --kutse 
 exec linnaotsing t;
 
+insert into opilane(eesnimi, perekonnanimi, synniaeg, stip, keskmine_hinne)
+VALUES(
+'Adriana',
+'Pikaljov',
+'2007-07-26',
+1,
+4.5),
+('Adri',
+'sss',
+'2005-06-11',
+1,
+2.2),
+('sdri',
+'bbb',
+'2004-06-24',
+1,
+3.3),
+('ddri',
+'lll',
+'2009-04-23',
+1,
+5.2),
+('bdri',
+'ooo',
+'2000-06-13',
+1,
+2.7)
+--protseduur mis lisab opilasi
+create procedure lisaopilaseandmed
+@eesnimi varchar(25),
+@perekonnanimi varchar(25),
+@synniaeg date
+as
+begin
+
+insert into opilane(eesnimi, perekonnanimi, synniaeg)
+values  (@eesnimi, @perekonnanimi,@synniaeg);
+select * from opilane
+
+end;
+-- protseduuri kutse 
+
+exec lisaopilaseandmed 'nikita', 'üüü', '2002-08-09'
+--protseduur mis kustutab opilasi id järgi
+create procedure opilastekustutamine
+@deleteId int 
+as
+begin
+select * from opilane;
+delete from opilane where opilaneId=@deleteId;
+select * from opilane;
+end;
+
+exec opilastekustutamine 3;
+--protseduur, mis otsib opilasi esimese tahe jargi 
+create procedure opilastenimeotsing
+@taht char(1)
+as
+begin 
+select * from opilane
+where eesnimi like @taht + '%';
+-- % - koik teised tahed 
+end;
+--kutse 
+exec opilastenimeotsing d;
+
+create procedure stipvuuendus 
+@opilaneId int, 
+@koef decimal(2,1)
+as
+begin
+select * from opilane;
+update opilane set stip=stip*@koef
+WHERE opilaneId=@opilaneId;
+select * from opilane;
+end;
+--kutse
+exec stipvuuendus 1, 1.2;
+
+
+
+drop procedure stipvuuendus;
+--protseduur mis uuendab keskhinned
+create procedure khinneuuendus
+@opilaneId int, 
+@koef decimal(2,1)
+as
+begin
+select * from khinneuuendus;
+update opilane set keskmine_hinne=keskmine_hinne*@koef
+WHERE opilaneId=@opilaneId;
+select * from opilane;
+end;
+--kutse
+exec khinneuuendus 1, 1.2;
+--protseduuri kustutamine
+drop procedure khinneuuendus;
+--keskmise hinne uuendamine
+
+--protseduur mis uuendab keskhinned
+create procedure khinneuuendus
+@opilaneId int, 
+@koef decimal(2,1)
+as
+begin
+select * from khinneuuendus;
+update opilane set keskmine_hinne=keskmine_hinne*@koef
+WHERE opilaneId=@opilaneId;
+select * from opilane;
+end;
+--kutse
+exec khinneuuendus 2, 1.2;
+
+--tabeli uuendamine - rahvaarv kasvab 10% võrra
+update opilane set keskmine_hinne=keskmine_hinne*1.1
+select * from opilane;
+update opilane set keskmine_hinne=keskmine_hinne*1.1
+WHERE opilaneId=2;
+
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 kasutame XAMPP/ localhost
+create table linn(
+linnId int primary key AUTO_INCREMENT,
+linnanimi varchar(30),
+rahvaarv int);
+insert into linn (linnanimi, rahvaarv)
+values  ('paide', 8888888), ('pärnu', 7777777);
