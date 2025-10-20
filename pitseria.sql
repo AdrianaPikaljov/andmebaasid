26f2d1b1184841b3d28243a66b5e5fbeb470122d
adri <pikaljovadri@gmail.com>
2025-10-20 12:28:41 +0300

    Create database and update tables for order management
    Added new database structure and triggers for order management.

--- pitseria.sql
+++ pitseria.sql
@@ -1,31 +1,39 @@
-CREATE TABLE tellimusse_rida (
-    tellimuserida_ID INT PRIMARY KEY,
-    kogus INT,
-    pizzamenu_pizzaID INT,
-    tellimusused_tellimus_ID INT,
-    FOREIGN KEY (pizzamenu_pizzaID) REFERENCES pizzamenu(pizzaID),
-    FOREIGN KEY (tellimusused_tellimus_ID) REFERENCES tellimusused(tellimus_ID)
-);
+create database pitseria2
+use pitseria2
 
 CREATE TABLE pizzamenu (
-    pizzaID INT PRIMARY KEY,
+    pizzaID INT PRIMARY KEY IDENTITY(1,1),
     nimi VARCHAR(30),
-    hind VARCHAR(10)
+    hind DECIMAL(10, 2)
 );
 
-CREATE TABLE tellimusused (
-    tellimus_ID INT PRIMARY KEY,
-    tellimus_aeg DATETIME
+CREATE TABLE tellimuse_staatus (
+    staatus_ID INT PRIMARY KEY,
+    nimetus VARCHAR(20) UNIQUE
 );
+
+CREATE TABLE tellimused (
+    tellimus_ID INT PRIMARY KEY IDENTITY(1,1),
+    tellimus_aeg DATETIME,
+    staatusID INT,
+    FOREIGN KEY (staatusID) REFERENCES tellimuse_staatus(staatus_ID)
+);
+
 
 CREATE TABLE klientid (
-    klienti_ID INT PRIMARY KEY,
+    klienti_ID INT PRIMARY KEY IDENTITY(1,1),
     nimi VARCHAR(30),
     telefon VARCHAR(30)
 );
 
+CREATE TABLE tootajad (
+    tootaja_ID INT PRIMARY KEY IDENTITY(1,1),
+    tootajanimi VARCHAR(30),
+    roll VARCHAR(30)
+);
+
 CREATE TABLE kohaletoimetamine (
-    kohaletoimetamine_ID INT PRIMARY KEY,
+    kohaletoimetamine_ID INT PRIMARY KEY IDENTITY(1,1),
     address VARCHAR(35),
     kohaletoimetamise_tyyp VARCHAR(20),
     tootajad_tootaja_ID INT,
@@ -34,17 +42,84 @@ CREATE TABLE kohaletoimetamine (
     FOREIGN KEY (klientid_klienti_ID) REFERENCES klientid(klienti_ID)
 );
 
-CREATE TABLE tootajad (
-    tootaja_ID INT PRIMARY KEY,
-    tootajanimi VARCHAR(30),
-    roll VARCHAR(30)
+CREATE TABLE tellimusse_rida (
+    tellimuserida_ID INT PRIMARY KEY IDENTITY(1,1),
+    kogus INT,
+    pizzamenu_pizzaID INT,
+    tellimusused_tellimus_ID INT,
+    FOREIGN KEY (pizzamenu_pizzaID) REFERENCES pizzamenu(pizzaID),
+    FOREIGN KEY (tellimusused_tellimus_ID) REFERENCES tellimusused(tellimus_ID)
 );
 
 CREATE TABLE makse (
-    makse_ID INT PRIMARY KEY,
-    naksevis VARCHAR(30),
-    makse_aeg INT,
-    summa VARCHAR(10),
+    makse_ID INT PRIMARY KEY IDENTITY(1,1),
+    makseviis VARCHAR(30),
+    makse_aeg DATETIME,
+    summa DECIMAL(10, 2),
     tellimusused_tellimus_ID INT,
     FOREIGN KEY (tellimusused_tellimus_ID) REFERENCES tellimusused(tellimus_ID)
 );
+
+
+CREATE TABLE logi (
+    logiID INT PRIMARY KEY IDENTITY(1,1),
+    aeg DATETIME,
+    toiming varchar(50),
+    andmed varchar(400),
+    kasutaja  varchar(100)
+);
+
+
+
+create trigger tellimuselisamine
+on tellimused
+for insert
+as
+begin
+    insert into logi (aeg, kasutaja, toiming, andmed)
+    select
+        getdate(),
+        system_user,
+        'tellimus lisatud',
+        concat('staatus: ', tellimuse_staatus.nimetus, ', aeg: ', inserted.tellimus_aeg, ', tellimus id: ', inserted.tellimus_ID)
+    from inserted
+    inner join tellimuse_staatus on inserted.staatusID = tellimuse_staatus.staatus_ID;
+end;
+
+
+
+
+create trigger tellimusemuudatus
+on tellimused
+for update
+as
+begin
+    insert into logi (aeg, kasutaja, toiming, andmed)
+    select
+        getdate(),
+        system_user,
+        'tellimus on uuendatud',
+		concat('vanad andmed: aeg: ', deleted.tellimus_aeg, ' ||| staatus: ', tellimuse_staatus.nimetus,' ||| tellimus id: ', inserted.tellimus_ID,' |||')
+    from deleted
+    inner join inserted on deleted.tellimus_ID = inserted.tellimus_ID
+    inner join tellimuse_staatus on inserted.staatusID = tellimuse_staatus.staatus_ID;
+end;
+
+
+create trigger tellimusekustutus
+on tellimused
+for delete
+as
+begin
+    insert into logi (aeg, kasutaja, toiming, andmed)
+    select
+        getdate(),
+        system_user,
+        'tellimus on kustutatud',
+        concat('kustutatud tellimus: ', deleted.tellimus_ID, ' | staatus: ', tellimuse_staatus.nimetus, ' | tellimuse aeg: ', deleted.tellimus_aeg)
+    from deleted
+    inner join tellimuse_staatus on deleted.staatusID = tellimuse_staatus.staatus_ID;
+end;
+
+
+select * from tellimused
